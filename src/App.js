95180bfe732cbb1585f493810340550bac1b828c
Shujaat Azim <shujaat.azim@gmail.com>
2019-11-04 19:08:55 -0500

    added controlled forms, got full functionality fo adding tips and restaurants to backend and to the frontend without refreshing the page, removed unused CSS files since this is just a practice frontend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import './App.css';
 
 class App extends React.Component {
 
   state = {
     tips: [],
-    restaurants: []
+    restaurants: [],
+    restaurantName: "",
+    restaurantLocation: "",
+    tipAmount: "",
+    tipDate: "",
+    tipLocation: ""
   }
 
   componentDidMount() {
@@ -21,22 +25,41 @@ class App extends React.Component {
       restaurants: data
     }))
   }
+
+  getRestaurants = () => {
+    fetch('http://localhost:3000/restaurants')
+    .then(resp => resp.json())
+    .then(restaurants => this.setState({restaurants}))
+  }
   
-handleNewRestaurant = (event) => {
-  fetch('http://localhost:3000/restaurants', {
-    method:"POST",
-    headers: {
-      "Accept": "application/json",
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      name: event.target.name.value,
-      location: event.target.location.value
+  getTips = () => {
+    fetch('http://localhost:3000/tips')
+    .then(resp => resp.json())
+    .then(tips => this.setState({tips}))
+  }
+
+  handleNewRestaurant = (event) => {
+    event.preventDefault()
+    fetch('http://localhost:3000/restaurants', {
+      method:"POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: this.state.restaurantName,
+        location: this.state.restaurantLocation
+      })
     })
-  })
-}
+    .then(this.getRestaurants)
+    .then(this.setState({
+      restaurantName: "",
+      restaurantLocation: ""
+    }))
+  }
 
 handleNewTip = (event) => {
+  event.preventDefault()
   fetch('http://localhost:3000/tips', {
     method: "POST",
     headers: {
@@ -44,22 +67,50 @@ handleNewTip = (event) => {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
-      amount: event.target.amount.value,
-      date: event.target.date.value,
-      restaurant_id: event.target.restaurant_id.value
+      amount: this.state.tipAmount,
+      date: this.state.tipDate,
+      restaurant_id: this.state.tipLocation
     })
   })
+  .then(this.setState({
+    tipAmount: "",
+    tipDate: "",
+    tipLocation: ""
+  }))
+  .then(this.getTips)
 }
 
 handleRestaurantDelete = (event) => {
-  fetch(`http://localhost:3000/restaurants/${event.currentTarget.parentNode.id}`, {
-    method: "DELETE"
+  const deletedRestaurant = event.currentTarget.parentNode.id
+  fetch(`http://localhost:3000/restaurants/${deletedRestaurant}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      "Accepts": "application/json"
+    },
+    body: JSON.stringify(deletedRestaurant)
   })
+  .then(this.getRestaurants)
 }
 
 handleTipDelete = (event) => {
-  fetch(`http://localhost:3000/tips/${event.currentTarget.parentNode.id}`, {
-    method: "DELETE"
+  const deletedTip = event.currentTarget.parentNode.id
+  fetch(`http://localhost:3000/tips/${deletedTip}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      "Accepts": "application/json"
+    },
+    body: JSON.stringify(deletedTip)
+  })
+  .then(this.getTips)
+}
+
+changeForm = (event) => {
+  const name = event.target.name
+  const value = event.target.value
+  this.setState({
+    [name]: value
   })
 }
 
@@ -68,11 +119,10 @@ handleTipDelete = (event) => {
       <div>
         <div>
           <form onSubmit={this.handleNewRestaurant}>
-            Add new restaurant!
+            <h2>Add new restaurant!</h2>
+            Name: <input type="text" name="restaurantName" value={this.state.restaurantName} onChange={this.changeForm}/>
             <br />
-            Name: <input type="text" name="name" />
-            <br />
-            Location: <input type="text" name="location" />
+            Location: <input type="text" name="restaurantLocation" value={this.state.restaurantLocation} onChange={this.changeForm}/>
             <br />
             <input type="submit" />
           </form>
@@ -80,13 +130,12 @@ handleTipDelete = (event) => {
         <br />
         <div>
           <form onSubmit={this.handleNewTip}>
-            Add a new tip!
+            <h2>Add a new tip!</h2>
+            Amount: <input type="text" value={this.state.tipAmount} name="tipAmount" onChange={this.changeForm} />
             <br />
-            Amount: <input type="text" name="amount" />
+            Date: <input type="text" value={this.state.tipDate} name="tipDate" onChange={this.changeForm} />
             <br />
-            Date: <input type="text" name="date" />
-            <br />
-            Restaurant ID: <input type="number" name="restaurant_id" />
+            Restaurant ID: <input type="number" value={this.state.tipLocation} name="tipLocation" onChange={this.changeForm} />
             <br />
             <input type="submit" />
           </form>
